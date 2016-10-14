565d8e031bcb1b8b36d9f91a347bdb4fabfe0c7e
RodrigoUrrutiaC <luisrurrutiac@gmail.com>
2016-10-14 08:41:23 -0600

    Ejercicio 1 y 2 terminados y clase AlarmaEmergencia agregada

--- src/AlarmaMain.java
+++ src/AlarmaMain.java
@@ -3,10 +3,10 @@
 
 public class AlarmaMain {
 	public static void main(String[] args) {
-		AlarmaEmergencia alarmaIncendio = new AlarmaEmergencia();
-		alarmaIncendio.m1(); // emergencia 1
-		alarmaIncendio.m2(); // alarma 1 / timbre 1
-		System.out.println(alarmaIncendio); // emergencia Ring...Ring...Ring...Ring
+		Alarma miReloj = new Alarma();
+		System.out.println(miReloj);
+		miReloj.m1();
+		miReloj.m2();
 		
 	}
 }
